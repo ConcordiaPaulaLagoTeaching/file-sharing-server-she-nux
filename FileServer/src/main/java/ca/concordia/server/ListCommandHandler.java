68b41c7cd279546bc3964bfa0cf3ce9e0b2b1103
DessiTech <djess.ndjiongue@gmail.com>
2025-11-15 07:17:42 -0500

    Implemented file system commands: CREATE, READ, WRITE, DELETE, LIST; resolved merge conflicts

--- FileServer/src/main/java/ca/concordia/server/ListCommandHandler.java
+++ FileServer/src/main/java/ca/concordia/server/ListCommandHandler.java
@@ -2,9 +2,22 @@ package ca.concordia.server;
 
 import java.io.PrintWriter;
 
+import ca.concordia.filesystem.FileSystemManager;
+
 public class ListCommandHandler {
+    //Djessica
+    // modifying so that it actually calls FileSystemManager.listFiles()
+
+    // Get the singleton instance of FileSystemManager
+    private static FileSystemManager fsManager = FileSystemManager.getInstance(null, 0);
+
     public static void handle(PrintWriter writer) {
-        writer.println("TODO: List files from FileSystemManager");
+        
+        // Get the list of files
+        String files = fsManager.listFiles();
+
+        // Send it to the client
+        writer.println(files);
         writer.flush();
     }
 }
