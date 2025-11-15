f317d2a9c1a2a76d5909ea205b0f15bc81e2ea18
DessiTech <djess.ndjiongue@gmail.com>
2025-11-14 20:02:42 -0500

    Implement singleton fix + server/client test working

--- FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
+++ FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
@@ -9,7 +9,10 @@ public class FileSystemManager {
 
     private final int MAXFILES = 5;
     private final int MAXBLOCKS = 10;
-    private final static FileSystemManager instance;
+
+    //Djessica
+    private static FileSystemManager instance = null;//not final
+
     private final RandomAccessFile disk;
     private final ReentrantLock globalLock = new ReentrantLock();
 
@@ -18,13 +21,23 @@ public class FileSystemManager {
     private FEntry[] inodeTable; // Array of inodes
     private boolean[] freeBlockList; // Bitmap for free blocks
 
+
+    //Djessica
+    //Made the constructor private.
+    //Removed the instance check — singleton is now handled in getInstance() (at the bottom)
+
     public FileSystemManager(String filename, int totalSize) {
-        // Initialize the file system manager with a file
-        if(instance == null) {
-            //TODO Initialize the file system
-        } else {
-            throw new IllegalStateException("FileSystemManager is already initialized.");
-        }
+        // // Initialize the file system manager with a file
+        // if(instance == null) {
+        //     //TODO Initialize the file system
+        // } else {
+        //     throw new IllegalStateException("FileSystemManager is already initialized.");
+        // }
+
+          this.disk = null; // TODO: initialize RandomAccessFile with filename
+    this.inodeTable = new FEntry[MAXFILES];
+    this.freeBlockList = new boolean[MAXBLOCKS];
+    // TODO: initialize FS
 
     }
 
@@ -33,16 +46,12 @@ public class FileSystemManager {
         throw new UnsupportedOperationException("Method not implemented yet.");
     }
 
-<<<<<<< HEAD
-
-=======
-    public static FileSystemManager getInstance(String filename, int totalsize){
-        if (instance == null){
-            instance = new FileSystemManager(null, totalsize);
-        }
-        return instance;
+    public static synchronized FileSystemManager getInstance(String filename, int totalSize) {
+    if (instance == null) {
+        instance = new FileSystemManager(filename, totalSize);
     }
-    
->>>>>>> 5ac88b7 (Add getInstance() to FileSystemManager singleton)
+    return instance;
+}
+
     // TODO: Add readFile, writeFile and other required methods,
 }
